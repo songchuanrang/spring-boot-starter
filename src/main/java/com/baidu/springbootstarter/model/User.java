e79f881aca6df5079b88496c151298263787b596
songchuanrang <scr0419@outlook.com>
2019-05-21 14:31:14 +0800

    增加lombok

--- src/main/java/com/baidu/springbootstarter/model/User.java
+++ src/main/java/com/baidu/springbootstarter/model/User.java
@@ -1,50 +1,11 @@
 package com.baidu.springbootstarter.model;
 
-public class User {
+import lombok.Data;
 
+@Data
+public class User {
     private int id;
     private String username;
     private String password;
     private int age;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
